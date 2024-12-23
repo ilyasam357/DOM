2381b871e36019b8578b99c4c553602eea05ea70
ilyasam357 <ilyasam357@gmail.com>
2024-12-23 08:55:37 +0700

    edit and add file

---
+++ practic_code/script.js
@@ -0,0 +1,11 @@
+// manipulasi atribut dengan setAttribute
+// const img = document.getElementById('gambar'); // Mengambil elemen gambar
+
+// img.setAttribute('width', 300); //menanipulasi atribut dengan setAttribute
+// img.setAttribute('height', 215);
+
+// const btns = document.querySelectorAll('.button'); // Mengambil semua elemen button
+// const playBtn = btns[3]; // Mengambil elemen div button play
+// const playBtnElement = playBtn.children[0]; // Mengambil elemen button play
+
+// playBtnElement.setAttribute('type', 'submit');
